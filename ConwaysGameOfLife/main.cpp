e77f3091b96dac1f85fdeaf45e04715afe15456b
Olechiw <jakob.olechiw@gmail.com>
2023-06-12 17:50:50 -0400

    Cleaned up main slightly

--- ConwaysGameOfLife/main.cpp
+++ ConwaysGameOfLife/main.cpp
@@ -33,9 +33,10 @@ ConwayGame getDefaultGame()
 
 int main(int argc, char *argv[])
 {
+    ApplicationModel* appModel = new ApplicationModel;
+
     qmlRegisterType<ConwayCanvas>("Conway", 1, 0, "ConwayCanvas");
     qmlRegisterType<Meter>("Conway", 1, 0, "Meter");
-    ApplicationModel* appModel = new ApplicationModel;
     qmlRegisterSingletonInstance<ApplicationModel>("Conway", 1, 0, "ApplicationModel", appModel);
 
     QGuiApplication app(argc, argv);
@@ -43,23 +44,24 @@ int main(int argc, char *argv[])
     QQmlApplicationEngine engine;
     engine.load(QUrl(QStringLiteral("qrc:/main.qml")));
     
-    ConwayCanvas* canvas = findChild<ConwayCanvas*>("conwayCanvas", engine);
-    QQuickWindow* window = findChild<QQuickWindow*>("mainWindow", engine);
+    QThread* workerThread = new QThread;
+    ConwayWorker* worker = new ConwayWorker(getDefaultGame(), appModel, workerThread);
 
-    QThread* thread = new QThread;
-    ConwayWorker* worker = new ConwayWorker(getDefaultGame(), appModel, thread);
-    thread->start();
+    // Bind state requesters
+    ConwayCanvas* canvas = findChild<ConwayCanvas*>("conwayCanvas", engine);
+    QObject::connect(canvas, &ConwayCanvas::requestLatestState, worker, &ConwayWorker::getState, Qt::DirectConnection);
 
     GameStatsReporter* reporter = new GameStatsReporter(appModel);
-    QObject::connect(canvas, &ConwayCanvas::requestLatestState, worker, &ConwayWorker::getState, Qt::DirectConnection);
     QObject::connect(reporter, &GameStatsReporter::requestLatestState, worker, &ConwayWorker::getState, Qt::DirectConnection);
-
-    // Bind Fps Meter
-    QObject::connect(window, &QQuickWindow::beforeRendering, appModel, &ApplicationModel::incrementFps);
 
     // Bind click handler
     QObject::connect(canvas, &ConwayCanvas::onClicked, worker, &ConwayWorker::setAlive);
-    
+
+    // Bind Fps Meter
+    QQuickWindow* window = findChild<QQuickWindow*>("mainWindow", engine);
+    QObject::connect(window, &QQuickWindow::beforeRendering, appModel, &ApplicationModel::incrementFps);
+
+    workerThread->start();
     return app.exec();
 }
 
